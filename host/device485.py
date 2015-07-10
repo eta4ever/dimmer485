d16cef1f034c5592af2e06f939c6050333dca671
eta4ever <arlorlo@ya.ru>
2015-07-10 23:54:51 +0300

    -

--- host/device485.py
+++ host/device485.py
@@ -11,27 +11,49 @@ CHANGE_ADDR = 30
 
 class Device:
 
-	def __init__(self, address, name, devtype, controls, priority, initial_regs, connection):
-		""" конструктор"""
+	def __init__(self, name, devtype):
+		""" конструктор основного объекта"""
 
-		self.address = address # адрес устройства
 		self.name = name # имя устройства
 		self.type = devtype # тип устройства
-		self.what_controls = controls # имя устройства, управляемого этим
-		self.control_priority = priority # приоритет управляющего устройства
-		self.registers = [0,0] # создание полей регистров
-		
-		# для реального устройства - записать значения регистров в устройство и поля,
-		# для виртуального - только в поля
-		if self.type != "virtual":
-			self.write_registers(initial_regs, connection) 
-		else:
-			self.registers = initial_regs
+
+	def init_as_hardware(self, address, init_regs, connection):
+		""" инициализация "железного" или виртуального устройства"""
+		self.address = address
+
+		if self.write_registers(init_regs, connection):
+			return 0xFF
+		else: 
+			return 0
+	
+	def init_as_controller(self, controls, priority):
+		""" инициализация как управляющего """
+		self.what_controls = controls
+		self.priority = priority
+
+	def init_as_timer(self, regs_on, regs_off, time_on, time_off):
+		""" инициализация таймера """
+
+		self.regs_on = regs_on
+		self.regs_off = regs_off
+
+		self.time_on = time.strptime(time_on, "%H:%M") # конвертация строки в struct_time по формату
+		self.time_off = time.strptime(time_off, "%H:%M") 
+
+		# преобразование в минуты для простого сравнения
+		# так работать в итоге проще, чем преобразовывать туда-сюда форматы времени и 
+		# учитывать дату
+		self.time_on = self.time_on.tm_hour * 60 + self.time_on.tm_min
+		self.time_off = self.time_off.tm_hour * 60 + self.time_off.tm_min
 
 	def write_registers(self, registers, connection):
 		""" запись регистров устройства, возвращает FF при успехе
 		и 00 при неправильном подтверждении или его отсутствии
 		записывает в поля только при успехе"""
+
+		# не работает для таймера
+		if self.type == "timer":
+			return 0
 
 		# для виртуального устройства - только в поля
 		if self.type == "virtual":
@@ -39,12 +61,15 @@ class Device:
 			return 0xFF
 
 		# в устройство
+
+		self.registers = [0,0]
+
 		connection.send([self.address, WRITE_REG, registers[0], registers[1]])
 		time.sleep(0.01)
 
 		# подтверждение и его проверка
 		ack_packet = connection.receive()
-		
+
 		# неполный прием или неверная контрольная сумма
 		if ack_packet == [0]: 
 			return 0
@@ -65,6 +90,10 @@ class Device:
 		""" чтение регистров из устройства,
 		записывает в поля и возвращает FF при успехе,
 		иначе возвращает 0"""
+
+		# не работает для таймера
+		if self.type == "timer":
+			return 0
 
 		# для виртуального устройства - просто возврат FF,
 		# так как поля всегда актуальны
@@ -97,10 +126,9 @@ class Device:
 		возвращает 0 при неудаче, при успехе записывает в поле
 		и возращает FF"""
 
-		# для виртуального - просто запись поля
-		if self.type == "virtual":
-			self.address = address
-			return 0xFF
+		# не работает для таймера и виртуального устройства
+		if self.type in ["virtual","timer"]:
+			return 0
 
 		# в устройство
 		connection.send([self.address, CHANGE_ADDR, address, 0])
@@ -132,6 +160,15 @@ class Device:
 
 		return self.name
 
+	def get_address(self):
+		""" получение адреса """
+
+		# не работает для таймера и виртуального устройства
+		if self.type in ["timer", "virtual"]:
+			return 0
+		else:
+			return self.address
+
 	def get_type(self):
 		""" получение типа устройства"""
 
@@ -142,70 +179,66 @@ class Device:
 
 		return self.what_controls
 
-
-	def priority(self):
+	def get_priority(self):
 		""" получение приоритета управляющего устройства"""
 
-		return self.control_priority
+		return self.priority
 
-class Timer:
-
-	def __init__(self, name, controls, priority, regs_on, regs_off, time_on, time_off):
-		"""конструктор"""
-
-		self.name = name
-		self.what_controls = controls
-		self.priority = priority
-		self.regs_on = regs_on
-		self.regs_off = regs_off
-
-		self.time_on = time.strptime(time_on, "%H:%M") # конвертация строки в struct_time по формату
-		self.time_off = time.strptime(time_off, "%H:%M") 
-
-	def check(self):
+	def check_time(self):
 		""" проверка времени. Если исполнитель в это время должен быть выключен,
 		возвращает 0, если включен - FF """
 
-		current = time.localtime()
+		# работает только для таймера
+		if self.type != "timer":
+			return 0
 
-		current_on = list(current) # чтобы можно было изменить
-		current_on[3] = self.time_on.tm_hour
-		current_on[4] = self.time_on.tm_min
-		current_on[5] = 0 # секунды
-		current_on = time.struct_time(tuple(current_on)) # обратно в struct
-		current_on = time.mktime(current_on) # и в секунды-с-начала-эпохи
+		current = time.localtime() # получение struct текущего времени
+		current = current.tm_hour * 60 + current.tm_min # преобразование в минуты для простого сравнения
 
-		current_off = list(current) # чтобы можно было изменить
-		current_off[3] = self.time_off.tm_hour
-		current_off[4] = self.time_off.tm_min
-		current_off[5] = 0 # секунды
-		current_off = time.struct_time(tuple(current_off)) # обратно в struct
-		current_off = time.mktime(current_off) # и в секунды-с-начала-эпохи
+		if self.time_off >= self.time_on:
+			# если время выключения больше времени включения - все просто,
+			# перехода на следующие сутки нет
+			
+			if current in range(self.time_on, self.time_off):
+				return 0xFF
+			else:
+				return 0
 
-		# если время включения больше времени включения, значит, попадание
-		# на переход через сутки
-
-		# НЕ ПОЙДЕТ, ЭТО БУДЕТ РАБОТАТЬ ТОЛЬКО ЕСЛИ ТЕКУЩЕЕ ВРЕМЯ ДО ПЕРЕХОДА!
-		if current_on > current_off:
-			current_off += 86400
-
-		current = time.mktime(current)
-
-		print(current_on, current, current_off)
-
-		if current in range(int(current_on), int(current_off)+1):
-			return 0xFF
 		else:
-			return 0x00
-
+			# обработка перехода на следующие сутки
+			if current <= 1439: # если текущее время в промежутке до 23:59 включительно
+				if current in range(self.time_on, 1440): # проверить левый интервал
+					return 0xFF
+				else:
+					return 0
+			else: 
+				if current in range(0, self.time_off): # проверить правый интервал
+					return 0xFF
+				else:
+					return 0
 
 	def get_regs_on(self):
+		""" возвращает значения регистров для состояния ВКЛ """
+
+		# работает только для таймера
+		if self.type != "timer":
+			return [0,0]
 		return self.regs_on
 
 	def get_regs_off(self):
+		""" возвращает значения регистров для состояния ВЫКЛ """
+
+		# работает только для таймера
+		if self.type != "timer":
+			return [0,0]
 		return self.regs_off
 
 	def controls(self):
+		""" возвращает имя управляемого устройства"""
+
+		# не работает для исполнительных устройств
+		if self.type in ["pwm", "relay"]:
+			return ""
 		return self.what_controls
 
 
--- host/host.py
+++ host/host.py
@@ -1,30 +1,28 @@
-from device485 import Device, Timer
+from device485 import Device
 from comm485 import Conn485
 import time, configparser
+
+# количество попыток чтения или записи в устройство, задержка между попытками
+ATTEMPTS = 8
+ATTEMPT_DELAY = 0.11
 
 # конфигурационный файл устройтв
 CONFIG_FILE = "cfg\\devices.cfg"
 
-# конфигурационный файл таймеров
-TIMERS_CONFIG_FILE = "cfg\\timers.cfg"
-
 # типы управляющих устройства
-CONTROL_TYPES = ["encoder", "switch", "virtual"]
-
-# список управляющих устройств 
-controllers = []
+CONTROL_TYPES = ["encoder", "switch", "virtual", "timer"]
 
 # типы исполнительных устройств
 EXEC_TYPES = ["pwm", "relay"]
 
+# типы "железных" устройств
+HARDWARE_TYPES = ["encoder", "switch", "pwm", "relay"]
+
+# список управляющих устройств 
+controllers = []
+
 # список исполнительных устройств
 executors = []
-
-# список таймеров
-timers = []
-
-# создание подключения
-conn = Conn485()
 
 # создание объектов устройств по конфигурационному файлу
 config = configparser.RawConfigParser()
@@ -33,54 +31,128 @@ for section in config.sections():
 	
 	time.sleep(0.1)
 
-	# парсинг секции конфигурационного файла 
-	address = config.getint(section, 'address')
+	# выбор общих параметров
 	name = config.get(section, 'name')
 	dev_type = config.get(section, 'type')
-	init_regs = [config.getint(section, 'reg1'), config.getint(section, 'reg2')]
-	controls = config.get(section, 'controls')
-	priority = config.getint(section, 'priority')
+
+	# создание устройства
+	device = Device(name, dev_type)
 
 	# определение устройства в управляющие или исполнительные
 	if dev_type in CONTROL_TYPES:
-		controllers.append(Device(address, name, dev_type, controls, priority, init_regs, conn))
-		print ('Created controller "%s" type "%s" with address %i (controls %s priority %i)' % (name, dev_type, address, controls, priority))
+		controls = config.get(section, 'controls')
+		priority = config.getint(section, 'priority')
+		device.init_as_controller(controls, priority)
+		controllers.append(device)
 
 	elif dev_type in EXEC_TYPES:
-		executors.append(Device(address, name, dev_type, controls, priority, init_regs, conn))
-		print ('Created executor "%s" type "%s" with address %i' % (name, dev_type, address))
-
+		executors.append(device)
+	
 	else:
-		print ('Error creating "%s": incorrect device type "%s"' % (name, dev_type))	
-	
-# создание объектов таймеров по конфигурационному фвйлу
-config = configparser.RawConfigParser()
-config.read(TIMERS_CONFIG_FILE)
-for section in config.sections():
-	
-	name = config.get(section, 'name')
-	controls = config.get(section, 'controls')
-	regs_on = [config.getint(section, 'reg1on'), config.getint(section, 'reg2on')]
-	regs_off = [config.getint(section, 'reg1off'), config.getint(section, 'reg2off')]
-	time_on = config.get(section, 'time_on')
-	time_off = config.get(section, 'time_off')
-	priority = config.getint(section, 'priority')
+		print ('Error creating "%s": incorrect device type "%s"' % (name, dev_type))
 
-	timers.append(Timer(name, controls, priority, regs_on, regs_off, time_on, time_off))
-	print ('Created timer "%s" controls "%s" priority %i %s - %s' % (name, controls, priority, time_on, time_off))
+	# инициализация железного устройства
+	if dev_type in HARDWARE_TYPES:
+		
+		# создание подключения
+		conn = Conn485()
+
+		address = config.getint(section, 'address')
+		init_regs = [config.getint(section, 'reg1'), config.getint(section, 'reg2')]
+		
+		for attempt in range(0, ATTEMPTS):
+			if device.init_as_hardware(address, init_regs, conn):
+				print("{} addr {:d} ok".format(name, address))
+				break
+			else:
+				print(".", end='')
+				# del conn
+				# conn = Conn485()
+				# time.sleep(ATTEMPT_DELAY)
+		else:
+			print("ERROR writing initial regs to {} with addr {:d}".format(name, address))
+
+		del conn
+
+	# инициализация таймера
+	if dev_type == "timer":
+		regs_on = [config.getint(section, 'reg1on'), config.getint(section, 'reg2on')]
+		regs_off = [config.getint(section, 'reg1off'), config.getint(section, 'reg2off')]
+		time_on = config.get(section, 'time_on')
+		time_off = config.get(section, 'time_off')		
+		device.init_as_timer(regs_on, regs_off, time_on, time_off)
 
 # формирование словаря вида "имя исполнителя : код в списке executors"
 executors_id_by_name = { executor.get_name() : executors.index(executor) for executor in executors}
 
-controllers[0].write_registers([100,1], conn)
-controllers[1].write_registers([100,0], conn)
+# инициализация словаря "код исполнителя: []" для дальнейшего заполнения управляющими
+# сюда не попадают исполнители, управляемые по таймеру!
+executors_controlled_by = {executors.index(executor): [] for executor in executors}
 
-dummy = input(">>>")
+# заполнение словаря "код исполнителя: [список кодов управляющих]"
+for controller in controllers:
+	executor_id = executors_id_by_name[controller.controls()]
+	executors_controlled_by[executor_id].append(controllers.index(controller))
+
+#dummy = input(">>>")
+# print (executors_controlled_by)
 
 try:
 
 	while True:
+
+		conn = Conn485()
+
+		for executor_id in range(0,len(executors)):
+
+			if len(executors_controlled_by[executor_id]) == 1:
+				# простой случай, один управляющий
+
+				curr_controller = controllers[executors_controlled_by[executor_id][0]]
+
+				# чтение регистров управляющего
+				for attempt_r in range(0,ATTEMPTS):
+					if curr_controller.read_registers(conn):
+						regs_to_executor = curr_controller.get_registers()
+
+						print("+", end="")
+
+						# при удачном чтении - запись регистров исполнителя
+						time.sleep(0.1)
+						for attempt_w in range(0, ATTEMPTS):
+							if executors[executor_id].write_registers(regs_to_executor,conn):
+
+								print("!")
+
+								break
+							else:
+								# del conn
+								# conn = Conn485()
+								time.sleep(0.1)
+						else:
+							print("ERROR writing {} addr {:d}".format(executors[executor_id].get_name(), executors[executor_id].get_address()))
+
+						break
+					
+					else:
+						# del conn
+						# conn = Conn485()
+						time.sleep(0.1)
+				else:
+					print("ERROR reading {} addr {:d}".format(curr_controller.get_name(), curr_controller.get_address()))
+
+
+			time.sleep(1)
+			del conn
+
+
+	# while True:
 		
+		# for executors in 
+
+
+
+
 		# for controller in controllers:
 
 		# 	# если не читаются регистры управляющего устройства, дергать соединение
@@ -104,26 +176,31 @@ try:
 
 		# 	time.sleep(1)
 
-		if timers[0].check():
+		
+
+
+
+	# 	if timers[0].check():
 			
-			curr_regs = timers[0].get_regs_on()
+	# 		curr_regs = timers[0].get_regs_on()
 
-		else:
+	# 	else:
 
-			curr_regs = timers[0].get_regs_off()
+	# 		curr_regs = timers[0].get_regs_off()
 
-		curr_executor = executors[executors_id_by_name[timers[0].controls()]]
+	# 	curr_executor = executors[executors_id_by_name[timers[0].controls()]]
 
-		while not (curr_executor.read_registers(conn)):
-			del conn
-			time.sleep(0.1)
-			conn = Conn485()
-			time.sleep(0.1)
+	# 	while not (curr_executor.read_registers(conn)):
+	# 		del conn
+	# 		time.sleep(0.1)
+	# 		conn = Conn485()
+	# 		time.sleep(0.1)
 			
-		if curr_executor.get_registers() != curr_regs:
-			curr_executor.write_registers(curr_regs, conn)
+	# 	if curr_executor.get_registers() != curr_regs:
+	# 		curr_executor.write_registers(curr_regs, conn)
 
-		time.sleep(1)
+	# 	time.sleep(1)
+	# pass
 
 finally:
 	if "conn" in locals(): # проверка существования conn в локальной области видимости
